e33abc20bbcc2e187bb23b2f048d4ed26874882b
DaisyNg <quynh.nguyen.thao123@gmail.com>
2025-05-26 01:46:28 +0700

    Update hub_coverage.py

--- hub_coverage.py
+++ hub_coverage.py
@@ -12,7 +12,7 @@ from shapely.ops import unary_union
 # --------------------------------------
 # Config: raw CSV URL from your GitHub repo
 # --------------------------------------
-CSV_URL = "https://raw.githubusercontent.com/thao-nguyen-10/hub-coverage/refs/heads/main/csv_network_lat_lon_final.csv"
+CSV_URL = "https://raw.githubusercontent.com/thao-nguyen-10/hub-coverage/refs/heads/main/csv_network_lat_lon_final_v2.csv"
 
 # --------------------------------------
 # Utility: Generate geodesic circle (polygon)
@@ -29,10 +29,10 @@ def geodesic_circle(center_lat, center_lon, radius_km, num_points=60):
 # Utility: Draw map with polygons
 # --------------------------------------
 def create_map(wards_df, radius_km, city_name):
-    # city_centers = {
-    #     "Hà Nội": [21.0285, 105.8542],
-    #     "TP. Hồ Chí Minh": [10.7769, 106.7009]
-    # }
+    city_centers = {
+        "Hà Nội": [21.0285, 105.8542],
+        "TP. Hồ Chí Minh": [10.7769, 106.7009]
+    }
 
     # Create GeoDataFrame from input DataFrame
     geometry = [Point(lon, lat) for lon, lat in zip(df['lon'], df['lat'])]
@@ -48,16 +48,21 @@ def create_map(wards_df, radius_km, city_name):
         city_poly = gpd.read_file("https://raw.githubusercontent.com/thao-nguyen-10/hub-coverage/refs/heads/main/hn.geojson").to_crs(epsg=3857)
     else:
         city_poly = gpd.read_file("https://raw.githubusercontent.com/thao-nguyen-10/hub-coverage/refs/heads/main/hcm.geojson").to_crs(epsg=3857)
-
+    
+    # Clip buffer area to city boundary
+    clipped = gpd.overlay(
+        gpd.GeoDataFrame(geometry=[buffer_union], crs=gdf_m.crs),
+        city_poly,
+        how="intersection"
+        )
+    
     # Compute intersection and area
-    intersection = buffer_union.intersection(city_poly.unary_union)
-    coverage_area = intersection.area
+    coverage_area = clipped.geometry.area.sum()
     city_area = city_poly.unary_union.area
     coverage_percent = (coverage_area / city_area) * 100
     
-    # map_center = city_centers.get(city_name, [16.0, 108.0])
-    # m = folium.Map(location=map_center, zoom_start=12)
-    m = folium.Map(location=[df['lat'].mean(), df['lon'].mean()], zoom_start=11)
+    map_center = city_centers.get(city_name, [16.0, 108.0])
+    m = folium.Map(location=map_center, zoom_start=12)
 
     for _, row in wards_df.iterrows():
         # folium.Circle(
@@ -125,5 +130,7 @@ if filtered_df.empty:
 else:
     # Create and display map
     ward_map, coverage = create_map(filtered_df, radius_km, city)
+    sale_coverage = filtered_df["sale"].sum() / df[df["city"] == city]["sale"].sum()
     st.metric("Coverage Area (%)", f"{coverage:.2f}%")
+    st.metric("Coverage Sale (%)", f"{sale_coverage:.2f}%")
     st_folium(ward_map, width=700, height=500)
